86109f6c5d12c46ad178b45c50863217b8a89f0d
Manuel Guth <manuel.guth@cern.ch>
2021-06-22 21:06:43 +0200

    fixing manage.py setting module

--- manage.py
+++ manage.py
@@ -9,7 +9,7 @@ def main():
        # If WEBSITE_HOSTNAME is defined as an environment variable, then we're running
     # on Azure App Service and should use the production settings.
     settings_module = "bmk_orders_togo.production" if 'WEBSITE_HOSTNAME' in os.environ else 'bmk_orders_togo.settings'
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bmk_orders_togo.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
