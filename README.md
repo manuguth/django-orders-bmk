51b38207fa7145a8e7b676c08b8d3002005d8d0e
Manuel Guth <manuel.guth@cern.ch>
2021-06-22 20:21:28 +0200

    some azure related fixes

---
+++ README.md
@@ -0,0 +1,12 @@
+# Bestellplattform Essen ToGo BMK Buggingen
+
+
+## Azure Settings
+### WebApp
+- ResourceGroup: DjangoPostgres-Order-ToGo
+- Name: bmk-bestellung
+- Runtime Stack: python3.8
+
+### PostSQL
+- Servername: bmk-bestellung-db
+- databasename: festessendb202107
--- bmk_orders_togo/wsgi.py
+++ bmk_orders_togo/wsgi.py
@@ -11,6 +11,16 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bmk_orders_togo.settings')
+
+# If WEBSITE_HOSTNAME is defined as an environment variable, then we're running
+# on Azure App Service and should use the production settings in production.py.
+settings_module = "bmk_orders_togo.production" if 'WEBSITE_HOSTNAME' in os.environ else 'bmk_orders_togo.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
+
+
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bmk_orders_togo.settings')
+
+# application = get_wsgi_application()
