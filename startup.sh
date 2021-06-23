6c969b97fac56146931ab53ef1780a4a615fb9cb
Manuel Guth <manuel.guth@cern.ch>
2021-06-23 15:08:27 +0200

    fix startup

--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
 echo de_DE ISO-8859-1 >> /etc/locale.gen && locale-gen
-gunicorn --bind=0.0.0.0 --timeout 600 <module>.wsgi
+gunicorn --bind=0.0.0.0 --timeout 600 bmk_orders_togo.wsgi
 # oryx build /tmp/zipdeploy/extracted -o /home/site/wwwroot --platform python --platform-version 3.8 -i /tmp/8d9363af8c5d407 --compress-destination-dir -p virtualenv_name=antenv --log-file /tmp/build-debug.log
