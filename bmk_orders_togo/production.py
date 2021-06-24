5eac7c5d0630878b90754a6f5aebf1a24e52925a
Manuel Guth <manuel.guth@cern.ch>
2021-06-24 18:02:06 +0200

    fixing aloowed hosts

--- bmk_orders_togo/production.py
+++ bmk_orders_togo/production.py
@@ -3,7 +3,7 @@ import os
 
 # Configure the domain name using the environment variable
 # that Azure automatically creates for us.
-ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME', "127.0.0.1"]
+ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME'], "127.0.0.1"
                  ] if 'WEBSITE_HOSTNAME' in os.environ else []
 
 # WhiteNoise configuration
